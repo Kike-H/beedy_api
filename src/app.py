72838812028e6dfa4699a430bd6227e7036de3b2
Manuel E. Hernandez <kicke10h@gmail.com>
2022-06-04 15:39:57 -0500

    Make connnectio to data base

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, responses
+from config.database import conn
 
 app = FastAPI()
 
 @app.get('/')
 async def docs():
+    ''' 
+    Redirect to the documentation of Beedy API
+    '''
     return responses.RedirectResponse('/docs')
