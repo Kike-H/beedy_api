49bab54a700b320d203de4ea342c9d9608d48658
Manuel E. Hernandez <kicke10h@gmail.com>
2022-06-05 09:31:40 -0500

    Fixing middleware

--- src/routes/middleware.py
+++ src/routes/middleware.py
@@ -6,4 +6,8 @@ async def catch_exception_middleware(request: Request, call_next):
     try:
         return await call_next(request)
     except Exception as e:
-        return Response(content=json.dumps(e.args), status_code=e.args[0]['status_code'] if 'status_code' in e.args[0] else 500, media_type="application/json")
+        try:
+            code =  e.args[0]['status_code']
+        except:
+            code = 500
+        return Response(content=json.dumps(e.args), status_code=code, media_type="application/json")
