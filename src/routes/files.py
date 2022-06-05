bf9245ff0679cc3b1c08e57005818327632cb5f1
Manuel E. Hernandez <kicke10h@gmail.com>
2022-06-05 14:09:18 -0500

    Make route to delete a folder

--- src/routes/files.py
+++ src/routes/files.py
@@ -1,14 +1,19 @@
-from fastapi import APIRouter, File, UploadFile
+from werkzeug.security import check_password_hash
 from os import getcwd, makedirs, path, rename, remove
+from types import NoneType
+from fastapi import APIRouter, File, UploadFile
+from shutil import rmtree
 
 from src.models.files import FileData
+from src.config.database import conn
 from src.models.folder import Folder
+from src.schemas.users import users
 
 files = APIRouter()
 
 ROOT_DIR = path.abspath(path.join(getcwd(), './files/'))
 
-@files.post('/files/create-new/folder', response_model=Folder, tags=['Files', 'Folder'])
+@files.post('/files/create-new/folder', response_model=Folder, tags=['Folder'])
 def create_new_folder(folder: Folder):
     uri = ROOT_DIR+'/'+folder.id+'-'+folder.name+'/'
     try:
@@ -18,7 +23,7 @@ def create_new_folder(folder: Folder):
     except:
         raise TypeError({"details":'this path already exists', "status_code": 400})
 
-@files.put('/files/update/folder/{new_name}', response_model=Folder, tags=['Files', 'Folder'])
+@files.put('/files/update/folder/{new_name}', response_model=Folder, tags=['Folder'])
 def update_folder(new_name:str, folder: Folder):
     new_uri = ROOT_DIR+'/'+folder.id+'-'+new_name+'/'
     rename(folder.path, new_uri)
@@ -26,9 +31,16 @@ def update_folder(new_name:str, folder: Folder):
     folder.path = new_uri
     return folder
 
+@files.delete('/files/remove/folder/', tags=['Folder'])
+def remove_folder(folder:Folder):
+    user = conn.execute(users.select().where(users.c.id==folder.id)).first()
+    if(type(user) != NoneType and check_password_hash(user.password, folder.password)):
+        rmtree(folder.path)
+        return folder.path
+    raise TypeError({"Details":'Cant delete the course', "status_code":400})
+
 @files.post('/files/create-new/file-{id}-{folder}', response_model=FileData, tags=['Files'])
 async def create_new_file(id:str, folder:str ,file: UploadFile = File(...)):
-    # TODO: terminated
     file_data = FileData(id=id, folder=folder, name=file.filename)
     file_data.generatePath(ROOT_DIR+'/')
     with open (file_data.path, "wb") as file_saved:
